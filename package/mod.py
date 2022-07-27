fc5411a3c90609f44e6160620d47afdbccfa877d
BBlittlebro <popo8810017269@gmail.com>
2022-07-27 14:04:54 +0800

    deque

--- package/mod.py
+++ package/mod.py
@@ -1,4 +1,5 @@
 import math
+from typing import OrderedDict
 
 print(math.pi)
 math.pi = 3.14
@@ -37,3 +38,80 @@ for food in foods:
 
 for food, num in food_counter.items():
     print(food, num)
+
+# Counter
+from collections import Counter
+breakfast = ['spam', 'spam', 'eggs', 'spam']
+breakfast_counter = Counter(breakfast)
+print(breakfast_counter)
+print(breakfast_counter.most_common())      
+print(breakfast_counter.most_common(1))         # 降冪排序，引數回傳前幾筆資料
+
+lunch = ['eggs', 'eggs', 'bacon']
+lunch_counter = Counter(lunch)
+print(lunch_counter)
+
+print("+: ", breakfast_counter + lunch_counter) # 用法跟集合一樣
+print("-: ", breakfast_counter - lunch_counter)
+print("&: ", breakfast_counter & lunch_counter)
+print("|: ", breakfast_counter | lunch_counter) #跟加法不同，選出最多的群組
+
+##################################
+
+# Stack + Queue = deque
+def palindrome(word):
+    from collections import deque
+    de = deque(word)
+    while len(de) > 1:
+        if de.popleft() != de.pop():
+            return False
+    return True
+
+print(palindrome("abcdcba"))
+
+##################################
+
+# pprint
+from pprint import pprint
+quotes = OrderedDict([('A', 1), ('B', 2), ('C', 3)])
+print(quotes)
+pprint(quotes)
+
+from random import randrange
+print(range(10))
+b = randrange(10)
+print(b)
+
+##################################
+
+#11.1
+import zoo
+zoo.hours()
+
+#11.2
+import zoo as menagerie
+menagerie.hours()
+
+#11.3
+from zoo import hours
+hours()
+
+#11.4
+from zoo import hours as info
+info()
+
+#11.5
+plain = {'A':1, 'B':2, 'C':3}
+print(plain)
+
+#11.6
+fancy = OrderedDict({'A':1, 'B':2, 'C':3})
+print(fancy)
+
+#11.7
+from collections import defaultdict
+dict_of_list = defaultdict(list)
+#dict_of_list = {}
+dict_of_list['a'].append("something for a")
+dict_of_list['a'].append("something for a2")
+print(dict_of_list)
