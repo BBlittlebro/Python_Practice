a3b39b77cf96057b50a8b3c13e0dd32bfb90aae8
BBlittlebro <popo8810017269@gmail.com>
2022-08-04 12:50:20 +0800

    timedelta

--- Datetime/clock.py
+++ Datetime/clock.py
@@ -1,5 +1,6 @@
 # Datetime - date
 from datetime import date
+from threading import local
 halloween = date(2022, 10, 31)
 print(halloween)
 print(halloween.year)
@@ -68,4 +69,57 @@ print(list(now[x] for x in range(9)))
 
 tm = time.mktime(now)   # 轉換回 epoch
 print(tm)           # 毫秒在 localtime() 沒有被保存
+
 #####################################
+# strftime() 轉成字串
+# strptime() 轉回tuple
+print()
+import time
+fmt = "It's %A, %Y, %B %d, local time %I:%M:%S %p"
+t = time.localtime()
+print(time.strftime(fmt, t))
+
+#####################################
+# 根據語言環境改變
+print()
+import locale
+from datetime import date
+halloween = date(2022, 10, 31)
+for lang_country in ['en_us', 'fr_fr', 'de_de', 'es_es', 'is_is']:
+    locale.setlocale(locale.LC_TIME, lang_country)
+    print(halloween.strftime('%A, %B %d'))
+
+# 查看 lang_country
+names = locale.locale_alias.keys()
+good_names = [name for name in names if len(name) == 5 and name[2] == '_']
+print(good_names[:10])
+
+#####################################
+# 13.1
+print()
+from datetime import date
+now = date.today()
+print(now, type(now))
+print(now.isoformat(), type(now.isoformat()))
+
+# 13.3
+print()
+now_str = now.isoformat()
+fmt = "%Y-%m-%d"                    # 要跟 str 格式一模一樣
+print(datetime.strptime(now_str, fmt))
+
+# 13.4
+print()
+my_birthday = date(1999, 10, 1)
+print(my_birthday)
+
+# 13.5
+print()
+print(my_birthday.weekday())        # Monday == 0
+print(my_birthday.isoweekday())     # Monday == 1
+
+# 13.6
+print()
+from datetime import timedelta
+party_day = my_birthday + timedelta(days=10000)
+print(party_day)
