584737cf31c6fcb79a7866fdf726b4d896a88608
BBlittlebro <popo8810017269@gmail.com>
2022-08-05 18:05:31 +0800

    377

--- LeetCode/Medium/377. Combination Sum IV.py
+++ LeetCode/Medium/377. Combination Sum IV.py
@@ -1,5 +1,6 @@
 class Solution:
     ans = 0
+    dp = {}
     
     def combinationSum4(self, nums: list[int], target: int) -> int:
         nums.sort()
@@ -22,7 +23,49 @@ class Solution:
             now_nums.pop()
 
         return
+##################################################
+# DP 作法
+def combinationSum4(self, nums: list[int], target: int) -> int:
+	
+		# Sort the nums to break the loop when total - num becomes negative.
+        nums.sort()
+		# Allocates the dp table
+        dp = [0] * (target + 1)
+        # The case total becomes 0 is only 1 (nothing add)
+        dp[0] = 1
 
-    
+		# Try the all cases the total from 1 to target
+        for total in range(1, target + 1):
+			# Try all possible numbers.
+            for num in nums:
+                if total - num >= 0:
+					# Uses the previous information
+                    dp[total] += dp[total - num]
+                else:
+                    break
+        return dp[target]
+
+##################################################
+# dict 結合 DP
+class Solution:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        dp = {}
+        dp[0] = [1]   # {0: [1]}
+
+        def search(target):
+            if target < 0:
+                return
+            if target in dp:
+                return dp[target]
+            sum = 0
+            for num in nums:
+                if num < target:
+                    sum += search(target-num)
+                elif num == target:
+                    sum += 1
+            dp[target] = sum
+            return sum
+        return search(target)
+                
 obj = Solution()
-print(obj.combinationSum4([1,2,3], 4))
+print(obj.combinationSum4([4,2,1], 32))
